b747b48fc2de533d32dd6bd25c2f6eeeb1bcf2bb
Marcus Asare <165590698+marcusasar@users.noreply.github.com>
2025-02-24 00:00:30 +0000

    Delete SQL_Queries/fraud_transactions_percentage.sql

--- SQL_Queries/fraud_transactions_percentage.sql
+++
@@ -1,9 +0,0 @@
--- What percentage of total transactions are flagged as fraudulent? --	
-SELECT
-	ROUND(
-		COUNT(CASE WHEN fraud_flag = True THEN 1 END) 
-		  	/ COUNT(*)::NUMERIC(10,2) * 100, 2
-	) AS fruad_pcnt
-FROM
-	transactions_2025_01_17;
-	
