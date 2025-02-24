ea928ff3e5af91d588a5ea4a826793b163dddc15
Marcus Asare <165590698+marcusasar@users.noreply.github.com>
2025-02-24 00:01:39 +0000

    Create fraud_transactions_percentage.sql

---
+++ SQL_Queries/Fraudulent_transactions/fraud_transactions_percentage.sql
@@ -0,0 +1,9 @@
+-- What percentage of total transactions are flagged as fraudulent? --	
+SELECT
+	ROUND(
+		COUNT(CASE WHEN fraud_flag = True THEN 1 END) 
+		  	/ COUNT(*)::NUMERIC(10,2) * 100, 2
+	) AS fruad_pcnt
+FROM
+	transactions_2025_01_17;
+	
